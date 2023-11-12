b205239691425108774ccd9583869321be0a7b28
BuildTools <unconfigured@null.spigotmc.org>
2023-11-12 19:32:38 +0900

    1

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/insertS', async (req, res) => {
 
     res.send(`
       <script>
-        alert('예약이 완료되었습니다!');
+        alert('상담예약이 완료되었습니다!');
         window.location.href = '/';
       </script>
     `);
