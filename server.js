7a8dcc09112034f9af9c7639d0bf71028a7cfa0f
BuildTools <unconfigured@null.spigotmc.org>
2024-01-06 23:56:56 +0900

    1

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 80;
-const db = require('./db');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const db = require("./db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('img'));
+app.use(express.static("img"));
 app.use(cors());
 
-app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); })
-app.get('/advRes', (req, res) => { res.sendFile(__dirname + '/advRes.html'); })
-app.get('/conRes', (req, res) => { res.sendFile(__dirname + '/conRes.html'); })
-app.get('/admin', (req, res) => { res.sendFile(__dirname + '/admin.html'); })
-app.get('/slide.js', (req, res) => { res.sendFile(__dirname + '/slide.js'); })
-app.post('/reservation', async (req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/advRes", (req, res) => {
+  res.sendFile(__dirname + "/advRes.html");
+});
+app.get("/conRes", (req, res) => {
+  res.sendFile(__dirname + "/conRes.html");
+});
+app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/admin.html");
+});
+app.get("/slide.js", (req, res) => {
+  res.sendFile(__dirname + "/slide.js");
+});
+app.post("/reservation", async (req, res) => {
   const phone = req.body.phone + "-" + req.body.phone2 + "-" + req.body.phone3;
   let model = req.body.model;
   if (model == "other") model = req.body.othermodel;
   try {
-    const sql = 'INSERT INTO reservation (resTime, resKind, name, phone, birth, type, model, receive, storage, color) VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const values = [req.body.resKind, req.body.name, phone, req.body.birth, req.body.join_type, model, req.body.receive, req.body.storage, req.body.color];
+    const sql =
+      "INSERT INTO reservation (resTime, resKind, name, phone, birth, type, model, receive, storage, color) VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    const values = [
+      req.body.resKind,
+      req.body.name,
+      phone,
+      req.body.birth,
+      req.body.join_type,
+      model,
+      req.body.receive,
+      req.body.storage,
+      req.body.color,
+    ];
 
     await db.query(sql, values); // 쿼리 실행 및 결과 대기
 
@@ -44,43 +65,44 @@ app.post('/reservation', async (req, res) => {
   }
 });
 
-app.post('/admin', async (req, res) => {
+app.post("/admin", async (req, res) => {
   const password = req.body.password;
-  const correct_password = 'ha80558055!';
+  const correct_password = "ha80558055!";
   if (password === correct_password) {
     try {
-      const sql = 'SELECT name, phone, birth, type, model, storage ,color ,receive FROM reservation';
+      const sql =
+        "SELECT name, phone, birth, type, model, storage ,color ,receive FROM reservation";
       const result = await db.query(sql);
-      let table = '<table>';
-      table += '<tr>';
-      table += `<th>${'예약날짜'}</th>`;
-      table += `<th>${'예약방식'}</th>`;
-      table += `<th>${'성함'}</th>`;
-      table += `<th>${'전화번호'}</th>`;
-      table += `<th>${'생년월일'}</th>`;
-      table += `<th>${'변경유형'}</th>`;
-      table += `<th>${'희망모델'}</th>`;
-      table += `<th>${'저장용량'}</th>`;
-      table += `<th>${'색상'}</th>`;
-      table += `<th>${'수령방식'}</th>`;
-      table += '</tr>';
+      let table = "<table>";
+      table += "<tr>";
+      table += `<th>${"예약날짜"}</th>`;
+      table += `<th>${"예약방식"}</th>`;
+      table += `<th>${"성함"}</th>`;
+      table += `<th>${"전화번호"}</th>`;
+      table += `<th>${"생년월일"}</th>`;
+      table += `<th>${"변경유형"}</th>`;
+      table += `<th>${"희망모델"}</th>`;
+      table += `<th>${"저장용량"}</th>`;
+      table += `<th>${"색상"}</th>`;
+      table += `<th>${"수령방식"}</th>`;
+      table += "</tr>";
 
       result.forEach(function (item) {
-        table += '<tr>';
-        table += `<td>${resTime.resKind || ''}</td>`;
-        table += `<td>${item.resKind || ''}</td>`;
-        table += `<td>${item.name || ''}</td>`;
-        table += `<td>${item.phone || ''}</td>`;
-        table += `<td>${item.birth || ''}</td>`;
-        table += `<td>${item.type || ''}</td>`;
-        table += `<td>${item.model || ''}</td>`;
-        table += `<td>${item.storage || ''}</td>`;
-        table += `<td>${item.color || ''}</td>`;
-        table += `<td>${item.receive || ''}</td>`;
-        table += '</tr>';
+        table += "<tr>";
+        table += `<td>${resTime.resKind || ""}</td>`;
+        table += `<td>${item.resKind || ""}</td>`;
+        table += `<td>${item.name || ""}</td>`;
+        table += `<td>${item.phone || ""}</td>`;
+        table += `<td>${item.birth || ""}</td>`;
+        table += `<td>${item.type || ""}</td>`;
+        table += `<td>${item.model || ""}</td>`;
+        table += `<td>${item.storage || ""}</td>`;
+        table += `<td>${item.color || ""}</td>`;
+        table += `<td>${item.receive || ""}</td>`;
+        table += "</tr>";
       });
 
-      table += '</table>';
+      table += "</table>";
       res.send(`
         <html>
           <head>
---
+++ update.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 80;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
